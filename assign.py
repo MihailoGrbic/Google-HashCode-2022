a77dfe7ae68accaa398b061416f9f5803bf4827c
Mihailo_Grbic <mihailogrbic99@gmail.com>
2022-02-24 21:54:58 +0100

    Added mentorship

--- assign.py
+++ assign.py
@@ -1,11 +1,23 @@
+from audioop import reverse
+
+
 def assign_to_project(contributors, project):
     assignments = []
     active = [True for _ in contributors]
 
-    for role in project["roles"]:
+    mentored = [False for _ in project["roles"]]
+    for role_id, role in enumerate(project["roles"]):
         name, level = role
-        candidates = list(filter(lambda i: active[i] and name in contributors[i]["skills"] and contributors[i]["skills"][name] >= level, range(len(contributors))))
+
+        if mentored[role_id]:
+            if level == 1:
+                candidates = list(filter(lambda i: active[i], range(len(contributors))))
+            else:
+                candidates = list(filter(lambda i: active[i] and name in contributors[i]["skills"] and contributors[i]["skills"][name] >= level-1, range(len(contributors))))
+        else:
+            candidates = list(filter(lambda i: active[i] and name in contributors[i]["skills"] and contributors[i]["skills"][name] >= level, range(len(contributors))))
         candidates = sorted(candidates, key=lambda x: contributors[x]["availability"])
+        #candidates = sorted(candidates, key=lambda x: contributors[x]["skills"][name], reverse=True)
 
         if len(candidates) == 0:
             return False, []
@@ -13,15 +25,24 @@ def assign_to_project(contributors, project):
         idx = candidates[0]
         assignments.append(contributors[idx])
         active[idx] = False
-    
+
+        for role_id, role2 in enumerate(project["roles"]):
+            name2, level2 = role2
+            if name2 in contributors[idx]["skills"] and contributors[idx]["skills"][name2] >= level2:
+                mentored[role_id] = True
+
     next_available = max([x["availability"] for x in assignments]) + project["days"]
     for x in assignments:
         x["availability"] = next_available
 
     for assigned, role in zip(assignments, project["roles"]):
         name, level = role
-        if assigned["skills"][name] <= level:
-            assigned["skills"][name] += 1
+
+        if name in assigned["skills"]:
+            if assigned["skills"][name] <= level:
+                assigned["skills"][name] += 1
+        else:
+            assigned["skills"][name] = 1
 
     return True, assignments
 
