088ace971ac530672dd273dc988edf30f6597e85
Petar Nikić <petar.m.nikic@gmail.com>
2022-02-24 20:57:42 +0100

    Availability

--- assign.py
+++ assign.py
@@ -3,31 +3,33 @@ def assign_to_project(contributors, project):
     active = [True for _ in contributors]
 
     for role in project["roles"]:
-        success = False 
+        name, level = role
+        candidates = list(filter(lambda i: active[i] and name in contributors[i]["skills"] and contributors[i]["skills"][name] >= level, range(len(contributors))))
+        candidates = sorted(candidates, key=lambda x: contributors[x]["availability"])
 
-        for i in range(len(contributors)):
-            contributor = contributors[i]
-            if not active[i]:
-                continue
-
-            name, level = role
-            skills = contributor["skills"]
-            if name in skills and skills[name] >= level:
-                assignments.append(contributor)
-                active[i] = False
-                success = True
-                break
-        
-        if not success:
+        if len(candidates) == 0:
             return False, []
-                
+
+        idx = candidates[0]
+        assignments.append(contributors[idx])
+        active[idx] = False
+    
+    next_available = max([x["availability"] for x in assignments]) + project["days"]
+    for x in assignments:
+        x["availability"] = next_available
+
     return True, assignments
 
 def assign_to_projects(contributors, projects):
     project_assignments = []
+
+    for contributor in contributors:
+        contributor["availability"] = 0
+
     for project in projects:
         success, people = assign_to_project(contributors, project)
         if success:
             project_assignments.append((project["name"], people))
+        # print(" ".join([str(x["availability"]) for x in contributors]))
     return project_assignments
     
